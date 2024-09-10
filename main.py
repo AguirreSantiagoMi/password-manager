c5953d71cf1967054ddd0a8ac71fade3e98b6405
Aguirre Santiago <santiago.aguirre.mi@gmail.com>
2024-09-10 20:03:49 -0300

    main v3

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 RED = '#F1EF99'
 BACKGROUND_COLOR = '#FFE8C5'
 BUTTON_COLOR = '#FFA27F'
+
+# ---------------------------- SEARCH FUNCTIONALITY ------------------------------- #
+def find_password():
+    try:
+        with open('passwords.json', 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showerror(title='Not Found', message="No Data File Found")
+    else:
+        website = web_input.get().capitalize()
+        try:
+            messagebox.showinfo(title=website, message=f'Web: {website}\nEmail: {data[website]['email']}\nPassword: {data[website]['password']}')
+            data[website]
+            pyperclip.copy(data[website]['password'])
+        except KeyError:
+            messagebox.showerror(title='Not Found', message="No details for the website exists")
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
@@ -26,23 +43,41 @@ def generate_password():
     pyperclip.copy(password)
 
     pass_input.insert(0, password)
+    
+    
+    
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    website = web_input.get()
+    website = web_input.get().capitalize()
     user = user_input.get()
     password = pass_input.get()
+    new_data = {
+        website:{
+            'email': user,
+            'password': password
+        }
+    }
     
     if len(website) == 0 or len(password) == 0:
         messagebox.showwarning(title='Error', message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f'These are de data entered: \nEmail: {user} \nPassword: {password}')
-        
-        if is_ok:
-            with open('passwords.txt', mode='a') as file:
-                file.writelines(f'{website} | {user} | {password}\n')
-            
+        try:
+            with open('passwords.json', mode='r') as file:
+                #Reading old data
+                data = json.load(file)  
+                #Updating old data with new data
+                data.update(new_data)
+        except FileNotFoundError:
+            with open('passwords.json', mode='w') as file:
+                json.dump(new_data, file)      
+        else:
+            with open('passwords.json', mode='w') as file:
+                #Saving updated data
+                json.dump(data, file, indent=4)
+        finally:
             web_input.delete(0,'end')
             pass_input.delete(0,'end') 
+                
     
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk(screenName='Password Manager')
@@ -54,31 +89,44 @@ logo_img = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=0, row=0, columnspan=3)
 
-#Labels and inputs
+
+
+
+#Labels
 web_label = Label(text='Website:', bg=BACKGROUND_COLOR)
 web_label.grid(column=0, row=1)
-web_input = Entry(width=40, bg=RED)
-web_input.grid(column=1, row=1, columnspan=2)
 
-web_input.focus()
-#----
 user_label = Label(text='Email/Username:', bg=BACKGROUND_COLOR)
 user_label.grid(column=0, row=2)
-user_input = Entry(width=40, bg=RED)
-user_input.grid(column=1, row=2, columnspan=2)
 
-user_input.insert(0, "elsantydetodos1@gmail.com")
-#----
 pass_label = Label(text='Password:', bg=BACKGROUND_COLOR)
 pass_label.grid(column=0, row=3)
+
+
+
+#Inputs
+web_input = Entry(width=21, bg=RED)
+web_input.grid(column=1, row=1)
+web_input.focus()
+
+user_input = Entry(width=40, bg=RED)
+user_input.grid(column=1, row=2, columnspan=2)
+user_input.insert(0, "elsantydetodos1@gmail.com")
+
 pass_input = Entry(width=21, bg=RED,)
 pass_input.grid(column=1, row=3)
+
+
+
+#Buttons
 pass_generate_bt = Button(text='Generate Password', bg=BUTTON_COLOR, command=generate_password)
-pass_generate_bt.grid(column=2, row=3)
+pass_generate_bt.grid(column=2, row=3, pady=5)
 
 add_bt = Button(text='Add', bg=BUTTON_COLOR, width=33, command=save_password)
-add_bt.grid(column=1, row=4, columnspan=2)
+add_bt.grid(column=1, row=4, columnspan=2, pady=5)
 
+search_bt = Button(text='Search', bg=BUTTON_COLOR, width=14, command=find_password)
+search_bt.grid(column=2, row=1, pady=5)
 
 
 window.mainloop()
