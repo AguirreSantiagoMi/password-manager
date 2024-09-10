7fbf8333471ab2629a7d74e5b387f247dfe485d4
Aguirre Santiago <santiago.aguirre.mi@gmail.com>
2024-09-10 11:02:42 -0300

    Password generator

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 RED = '#d4483b'
 BACKGROUND_COLOR = '#E78895'
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+def generate_password():
+    pass_input.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    password_list = []
+
+    password_list += [choice(letters) for x in range(randint(8, 10))]
+    password_list += [choice(symbols) for x in range(randint(2, 4))]
+    password_list += [choice(numbers) for x in range(randint(2, 4))]
+    shuffle(password_list)
+
+    password = ''.join(password_list)
+    pyperclip.copy(password)
+
+    pass_input.insert(0, password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
     website = web_input.get()
@@ -51,9 +70,9 @@ user_input.insert(0, "elsantydetodos1@gmail.com")
 #----
 pass_label = Label(text='Password:', bg=BACKGROUND_COLOR)
 pass_label.grid(column=0, row=3)
-pass_input = Entry(width=21, bg=RED)
+pass_input = Entry(width=21, bg=RED,)
 pass_input.grid(column=1, row=3)
-pass_generate_bt = Button(text='Generate Password', bg='white')
+pass_generate_bt = Button(text='Generate Password', bg='white', command=generate_password)
 pass_generate_bt.grid(column=2, row=3)
 
 add_bt = Button(text='Add', bg='white', width=33, command=save_password)
